500a3b8b13de23830d517e9cc2c85d041c212668
Gabriel Lima <gabriel.rtlima@upe.br>
2022-05-11 10:51:12 -0300

    spring boot initializer

---
+++ src/test/java/br/upe/pweb/controlepeso/ControlePesoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.pweb.controlepeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePesoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
